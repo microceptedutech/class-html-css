8585caeb723d25cf97a2b7fac3acee723c24836c
Udipta Gogoi <gogoiudipta000@gmail.com>
2020-03-11 23:01:37 +0530

    if else

--- js/custom.js
+++ js/custom.js
@@ -2,18 +2,30 @@ var date1 = new Date();
 var day = date1.getDate();
 var month = date1.getMonth();
 var year = date1.getFullYear();
-if(month == 2){
-    month = 'March';
+// if(month == 2){
+//     month = 'March';
+// }
+// document.getElementById('date').innerHTML = day +" "+ month+" " + year;
+function submitForm(){
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var form_data = document.getElementById('form_data');
+    form_data.innerHTML = 'Name is <span class="text-danger">'+name+'</span> and email is <span class="text-danger">'+ email+'</span>';
 }
-document.getElementById('date').innerHTML = day +" "+ month+" " + year;
-
 function changeHtml(title){
     // document.getElementById('contact_title').innerHTML = title;
    var date = getDate();
    alert(date);
 }
-function getDate(){
-   return Date();
+function getDate(para){
+  var nmbr1 = 10;
+  var nmbr2 = 20;
+  if(para == 'add'){
+      alert(nmbr1+nmbr2);
+  }else if(para == 'sub'){
+      alert(nmbr2 - nmbr1);
+  }
+      
 }
 function alertPop(){
     alert('OK');
