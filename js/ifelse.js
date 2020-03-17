ae985ead782d7e9cd5fff7a9ad714ecd60412c3f
Udipta Gogoi <gogoiudipta000@gmail.com>
2020-03-17 22:35:58 +0530

    added ifelse.js

---
+++ js/ifelse.js
@@ -0,0 +1,16 @@
+var number = 200;
+var name = 'Micro';
+
+if(number == 10){
+    alert('The number is 10');
+}else if(number == 5){
+    alert('The number is 5');  
+}else if(number == 20){
+    alert('The number is 20');
+}else{
+    //alert('The number is neither 10 or 20 or 5');
+}
+
+if(name == 'Micro'){
+    alert('Name is Micro');
+}
